2cb3d5548518edce7b50357ce707b6495c12ee22
Yuririn <yuri.soushi@gmail.com>
2023-02-19 06:49:54 +0800

    サンプル:親コンポーネントから子コンポーネントに値を渡す

---
+++ js/components/PostItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <time datetime="post.date">{{setDateString}}</time><span>{{post.cate}}</span>{{ post.title }}
+</template>
+<script>
+import { computed } from "vue";
+export default {
+    props:{
+        post: { type: [Array, []] }
+    },
+    setup(props) {
+        const setDateString = computed(() =>{
+            return props.post.date.replace(/-/g, '/')
+        })
+        return {
+            setDateString
+        }
+    }
+}
+</script>
--- js/index.js
+++ js/index.js
@@ -1,120 +1,24 @@
-
 import {
   createApp,
   ref,
-  onMounted,
-} from "vue";
+} from 'vue';
 
-const jsonUrl = 'articles.json';
+import PostItem from './components/PostItem.vue';
 
 const app = createApp({
+  components:{ PostItem },
   setup() {
-    const articles = ref(null);
-    const paged = 10
-    const total = ref(null)
-    const currentPage = ref(1)
-    const maxPage = ref(null)
-    const tags = ref(null)
-    const selectedTags = ref([])
-    const sortDate = ref(null)
-    const message = ref(null)
 
-    const sortArticle = async () => {
-      let result;
-      result = await fetch( jsonUrl );
-      result = await result.json();
-
-      if(selectedTags.value.length !== 0) {
-        result = result.filter((i) => {
-          const article = i.tags.map(k => {
-            if(selectedTags.value.includes(k)) return true;
-          })
-          if(article.includes(true)) return i;
-        })
-      }
-
-      if(sortDate.value !== null) {
-        result = result.sort((a, b) => {
-          if(sortDate.value === 'desc') {
-            return (a.date < b.date ? 1 : -1);
-          } else if(sortDate.value === 'asc') {
-            return (a.date > b.date ? 1 : -1);
-          }
-        });
-      }
-      total.value = result.length;
-
-      maxPage.value = Math.ceil(total.value / paged)
-      articles.value  = result.slice( paged *  (currentPage.value - 1), paged * currentPage.value)
-      articles.value  = result.slice( paged *  (currentPage.value - 1), paged * currentPage.value)
-    }
-    const getTags = async () => {
-      let tagList;
-      tagList = await fetch( jsonUrl );
-      tagList = await tagList.json();
-      tagList = tagList.reduce((tags, article) => {
-        if (article.tags) {
-          article.tags.map(item => {
-            if (tags.find(i => i === item)) {
-              return false;
-            }
-            tags = [...tags, item]
-          })
-        }
-        return tags
-      },[])
-
-      tags.value = tagList
-    }
-    const sort = async () => {
-      currentPage.value = 1;
-      await sortArticle();
-      message.value = selectedTags.value.length !== 0 ? `${selectedTags.value.join(", ")} の記事が ${total.value} 件あります。` : null;
-    }
-    const clear = async () => {
-      currentPage.value = 1;
-      selectedTags.value = []
-      sortDate.value = null
-      message.value = null
-
-      await sortArticle();
-    }
-
-    const pagination = async (dir) => {
-      if(dir === 'prev') {
-        if (currentPage.value === 1) {
-          return;
-        }
-        currentPage.value--;
-      }
-      if(dir === 'next') {
-        if (currentPage.value === maxPage.value) {
-          return;
-        }
-        currentPage.value++;
-      }
-      await sortArticle();
-    };
-
-    onMounted( async () => {
-      await sortArticle();
-      await getTags();
-    })
+    const posts = ref(
+      [
+        { date : '2023-1-12', title:'カワサンの滝にネッシーが現れた！', cate: 'スクープ'},
+        { date : '2023-2-14', title:'バレンタインデーに幻のチョコはいかが？', cate: 'グルメ'},
+        { date : '2023-2-18', title:'ゴンザレス一家に7つ子誕生！ママ奮闘記', cate: '地域'}
+      ]
+    );
     return {
-      articles,
-      tags,
-      currentPage,
-      total,
-      maxPage,
-      selectedTags,
-      sortDate,
-      message,
-      pagination,
-      sortArticle,
-      sort,
-      clear
+      posts
     }
   }
 })
-
 app.mount("#app");
