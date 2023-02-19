d69a5bdd493d57ac471148be7fbc925d035a4707
Yuririn <yuri.soushi@gmail.com>
2023-02-19 09:22:23 +0800

    相互間のデータのやり取り

--- js/components/PostItem.vue
+++ js/components/PostItem.vue
@@ -1,5 +1,8 @@
 <template>
-   <label><input type="checkbox" v-model="votePost" :value="post.title"><time datetime="post.date">{{setDateString}}</time><span>{{post.cate}}</span>{{ post.title }}</label>
+   <label>
+    <input type="checkbox" v-model="votePost" :value="post.title">
+    <time datetime="post.date">{{setDateString}}</time>
+    <span>{{post.cate}}</span>{{ post.title }}</label>
 </template>
 <script>
 import { computed } from "vue";
